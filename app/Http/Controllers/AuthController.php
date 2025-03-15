e51c8f3811db911c287f149c01124bbaa7b8751a
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-15 14:50:18 +0000

    hero section

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,14 +29,27 @@ class AuthController extends Controller
         ]);
 
 
-        return redirect('/login')->with('success', 'Registration successful! Please log in.');
+        return redirect('login')->with('success', 'Registration successful! Please log in.');
 
     }
 
 
-    public function login(){
+    public function showLoginForm(){
         return view('/login');
     }
 
-    
+    public function login(Request $request){    
+     
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if(Auth::attempt(['email'=>$request->email ,'password'=>$request->password])){
+            return redirect()->route('/signup', ['id'=>Auth::id()]);
+        }
+        return view('/signup');
+    }
+  
+
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('register',[AuthController::class , 'showRegisterForm']);
 Route::post('register',[AuthController::class , 'register']);
 
-Route::get('login',[AuthController::class , 'login']);
+Route::get('login',[AuthController::class , 'showLoginForm']);
+Route::post('login',[AuthController::class , 'login']);
