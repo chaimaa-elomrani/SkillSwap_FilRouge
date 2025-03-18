daea6f2c2ca9030ffe455b33fccababd3bc1002b
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-18 15:25:09 +0000

    fixed the javascript link , and separation of the footer in an independent view

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,6 @@ use Illuminate\Support\Facades\Route;
 // Route::post('register',[AuthController::class , 'register']);
 
 Route::get('/',[HomeController::class , 'index']);
-// Route::get('login',[AuthController::class , 'showLoginForm']);
+Route::get('/header',[HomeController::class , 'header']);
+// Route::get('login',][AuthController::class , 'showLoginForm']);
 // Route::post('login',[AuthController::class , 'login']);
