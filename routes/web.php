8ce299e91f10a0ade532439972bb0072bdc8d4fc
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-21 01:26:48 +0000

    added root for the services page

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,18 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ServicesController;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('register',[AuthController::class , 'showRegisterForm']);
+//authentification
+Route::get('register',[AuthController::class , 'showRegisterForm']);
 // Route::post('register',[AuthController::class , 'register']);
+Route::get('login',[AuthController::class , 'showLoginForm']);
+// Route::post('login',[AuthController::class , 'login']);
 
+
+Route::get('/services',[ServicesController::class , 'index']);
 Route::get('/',[HomeController::class , 'index']);
 Route::get('/header',[HomeController::class , 'header']);
-// Route::get('login',][AuthController::class , 'showLoginForm']);
-// Route::post('login',[AuthController::class , 'login']);
+Route::get('/signup',[AuthController::class , 'showRegisterForm']);
