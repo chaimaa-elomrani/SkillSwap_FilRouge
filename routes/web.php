3b52477cafa11f4073d552979f65f66f7e322a2f
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-21 09:51:16 +0000

    added route for the search page 4

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,13 @@ use Illuminate\Support\Facades\Route;
 
 
 //authentification
-Route::get('register',[AuthController::class , 'showRegisterForm']);
 // Route::post('register',[AuthController::class , 'register']);
 Route::get('login',[AuthController::class , 'showLoginForm']);
 // Route::post('login',[AuthController::class , 'login']);
+Route::get('/signup',[AuthController::class , 'showRegisterForm']);
 
 
 Route::get('/services',[ServicesController::class , 'index']);
 Route::get('/',[HomeController::class , 'index']);
 Route::get('/header',[HomeController::class , 'header']);
-Route::get('/signup',[AuthController::class , 'showRegisterForm']);
+Route::get('/search',[ServicesController::class , 'index']);
