c3ba36d64cedc52e3e88a776d02399b2b1f3e8ca
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-03 00:29:14 +0000

    added the header of the admin user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,6 @@ class HomeController extends Controller
         return view('home');
     }
     public function header(){
-        return view('layout/users');
+        return view('layout/admin');
     }
 }
