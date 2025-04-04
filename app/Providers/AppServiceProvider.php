f67e4df29b0dde5421a9e679ae17cfe46c1be3f0
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-04 03:29:05 +0000

    added policies to implement users authaurization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
+use App\Policies\UserPolicy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        User::class => UserPolicy::class,
+    ];
     /**
      * Register any application services.
      */
@@ -21,4 +25,5 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
 }
