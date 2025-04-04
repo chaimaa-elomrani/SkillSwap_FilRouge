a964365c1da81394686f57b1af23136a76865256
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-04 03:53:53 +0000

    added the logout method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -71,4 +71,14 @@ class AuthController extends Controller
     }
 
 
+    public function logout(Request $request)
+    {
+        $user = Auth::user();
+        $user->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logout successful'
+        ]);
+    }
+
 }
