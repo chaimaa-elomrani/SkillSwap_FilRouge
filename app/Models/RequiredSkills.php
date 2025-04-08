ff0a350e9a81d1f712d61c527713561d243b6d59
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-08 16:08:27 +0100

    added the method find or create for the required skills

--- app/Models/RequiredSkills.php
+++ app/Models/RequiredSkills.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class skills_required extends Model
+class RequiredSkills extends Model
 {
     protected $fillable = ['name'];
 
---
+++ app/Services/RequiredSkillService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+use App\Models\Posts;
+use App\Models\RequiredSkills;
+use App\Models\skills_required; 
+
+
+class RequiredSkillService{
+
+    public function findOrCreate(string $name){
+        return RequiredSkills::firstOrCreate(['name' => trim($name)]);
+
+    }
+}
