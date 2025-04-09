1a26c1c801a7f305180ed3dca43e8bbd6c7efc54
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-09 20:14:42 +0100

    added the getskills and getDomains methods in the dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,29 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+
+    protected $skillsController;
+    protected $domainsController;
+    protected $languagesController;
+
+    public function __construct(SkillsController $skillsController, DomainController $domainsController, LanguagesController $languagesController)
+    {
+        $this->skillsController = $skillsController;
+        $this->domainsController = $domainsController;
+        $this->languagesController = $languagesController;
+    }
+
+
+    public function getSkills(){
+        $skills = $this->skillsController->index();
+        return $skills;
+    }
+
+    public function getDomains(){
+        $domains = $this->domainsController->index();
+        return $domains;
+    }
+
     
     public function index()
     {
