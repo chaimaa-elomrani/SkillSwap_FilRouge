fafa6acc5d35916448b3c9826c049c205965a19e
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-09 11:54:58 +0100

    added the CRUD of languages in the services

---
+++ app/Services/LanguagesService.php
@@ -0,0 +1,29 @@
+<?php 
+namespace App\Services;
+use App\Models\Languages;
+use Illuminate\Database\Eloquent\Collection;
+
+class LanguagesService{
+    
+    public function getAllLanguages(): Collection
+    {
+        return Languages::all();
+    }
+
+    public function create(array $data): Languages
+    {
+        return Languages::create($data);
+    }
+
+    public function update(Languages $language, array $data): bool
+    {
+        return $language->update($data);
+    }
+
+    public function delete(Languages $language): bool
+    {
+        return $language->delete();
+    }
+
+    
+}
