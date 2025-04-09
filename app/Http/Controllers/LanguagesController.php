465ae9549610799585eb9dcc6480a0e07c865e62
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-09 11:59:29 +0100

    added index method to get all languages and the store one

--- app/Http/Controllers/LanguagesController.php
+++ app/Http/Controllers/LanguagesController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\languages;
+use App\Services\LanguagesService;
 use Illuminate\Http\Request;
 
 class LanguagesController extends Controller
 {
-    //
+
+    protected $languagesService;
+    public function __construct(LanguagesService $languagesService){
+        $this->languagesService = $languagesService;
+    }
+
+
+    public function index(){
+        $languages = $this->languagesService->getAllLanguages();
+        return view('languages.index', compact('languages'));
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+        $this->languagesService->create([
+            'name' => $validated['name'],
+        ]);
+        return redirect()->route('languages.index');
+    }
+
+    
+   
 }
