8868ebd5cfd7f5013f74be878540f47976c1fe66
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-09 10:21:09 +0100

    added index function returning all the domains in the database in the controllers

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\DomainService;
 use Illuminate\Http\Request;
 
 class DomainController extends Controller
 {
-    //
+    
+    protected $domainService;
+
+    public function __construct(DomainService $domainService){
+        $this->domainService = $domainService;
+    }
+
+
+    public function index(){
+        $domains = $this->domainService->getAllDomains();
+        return view('domains.index', compact('domains'));
+    }
 }
