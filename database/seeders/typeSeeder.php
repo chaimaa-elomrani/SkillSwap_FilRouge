220699f863a7c44bb2d6d36e24ad9f753951173c
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-16 20:36:36 +0100

    added a seeder to fill the table of types

---
+++ database/seeders/typeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class typeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       $types =[
+        ['name'=>'Digital & Tech'],
+        ['name'=>'Creative'],
+        ['name'=>'Professional Services'],
+        ['name'=>'Lifestyle & Wellness'],
+        ['name'=>'Education & Learning'],
+        ['name'=>'Specialized Services'],
+        ['name'=>'Specialized Services'],
+        ['name'=>'Others'],
+       ];
+        DB::table('types')->insert($types);
+    }
+}
