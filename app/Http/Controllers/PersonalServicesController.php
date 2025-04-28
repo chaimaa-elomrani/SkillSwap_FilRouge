76547c8545c140eccca2e764b1c69b332072f6c9
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-28 02:26:20 +0100

    added the method of storing the services in the database

--- app/Http/Controllers/PersonalServicesController.php
+++ app/Http/Controllers/PersonalServicesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PersonalServices;
 use Illuminate\Http\Request;
 
 class PersonalServicesController extends Controller
@@ -9,5 +10,26 @@ class PersonalServicesController extends Controller
     public function getPersonalServicesbyUserId($userId){
         $user = auth()->user()->load('personalServices');
         return view('users/profile', compact('user'));
+    }
+
+    public function store(Request $request){
+ 
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:750',
+            'credit_cost' =>'required|numeric|min:0',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        
+        $services = PersonalServices::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'credut_cost' => $request->credit_cost,
+            'user_id' => auth()->id(),
+        ]);
+
+        return view('users/profile', compact('services'));
+
     }
 }
