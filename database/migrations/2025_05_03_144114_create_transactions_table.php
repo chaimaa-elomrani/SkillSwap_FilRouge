92c0600c61927c2efb11ff7a010ea9b212ac8456
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-05-03 16:07:51 +0100

    added the migrations of the transactions and credits table

--- database/migrations/2025_05_03_144114_create_transactions_table.php
+++ database/migrations/2025_05_03_144114_create_transactions_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
